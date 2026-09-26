10fa22ccd29c1348ea3e21e0173fb304dd4a3b22
christophernowa-bit <christophernowa@gmail.com>
2026-09-26 11:31:05 +0200

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { Navigate, Route, Routes } from 'react-router-dom'
+
+function Dashboard() {
+  return <h1>Dashboard</h1>
+}
+
+function Exams() {
+  return <h1>Exams</h1>
+}
+
+function Classes() {
+  return <h1>Classes</h1>
+}
+
+function Settings() {
+  return <h1>Settings</h1>
+}
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/exams" element={<Exams />} />
+      <Route path="/classes" element={<Classes />} />
+      <Route path="/settings" element={<Settings />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  )
+}
